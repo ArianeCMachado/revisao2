d8d2ceaf11a6b2aff16db24c7d7094ac4ecd85f7
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2024-04-02 20:57:28 -0300

    entrada_e_saida

--- entrada_e_saida.py
+++ entrada_e_saida.py
@@ -1,28 +1,27 @@
-
 def saudar_usuario():
     nome = input("Digite seu nome: ")
-    print("Olá," ,nome,"! Seja bem-vindo(a)!")
+    print("Olá,", nome, "! Seja bem-vindo(a)!")
 
 def soma():
-    num1 = (input("Digite o primeiro número: "))
-    num2 = (input("Digite o segundo número: "))
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
     resultado_soma = num1 + num2
     print("A soma dos números é:", resultado_soma)
 
 def multiplicacao():
-    num1 = (input("Digite o primeiro número: "))
-    num2 = (input("Digite o segundo número: "))
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
     resultado_multip = num1 * num2
     print("O produto dos números é:", resultado_multip)
 
 def divisao_por_dois():
-    num = (input("Digite um número: "))
+    num = float(input("Digite um número: "))
     resultado_div2 = num / 2
     print("A divisão do número", num, "por dois é:", resultado_div2)
 
 def calcular_imc():
-    altura = (input("Digite sua altura em metros: "))
-    peso = (input("Digite seu peso em quilogramas: "))
+    altura = float(input("Digite sua altura em metros: "))
+    peso = float(input("Digite seu peso em quilogramas: "))
     imc = peso / (altura ** 2)
     print("O seu IMC é:", imc)
 
